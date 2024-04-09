282777def90ef243feffb0a09db07fbeead6fec9
JorgeDam26 <jherreram13@educastillalamancha.es>
2024-04-09 10:55:05 +0200

    Jorge Herrera Mi quitno commit

--- src/FUtil.java
+++ src/FUtil.java
@@ -1,6 +1,13 @@
 import java.io.File;
 
 public class FUtil {
+  /**
+   * Es un método que te lee un fichero que te comprueba si 
+   *  existe (devuelve true)
+   *  no existe (devuelve false)
+   * @param filename
+   * @return
+   */
   public static boolean existe(String filename){
     File file = new File(filename);
     
